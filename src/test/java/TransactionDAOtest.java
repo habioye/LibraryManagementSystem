e80884904517bd66ce33d3ad7bf108b1eb83324e
habioye <78454514+habioye@users.noreply.github.com>
2024-06-06 14:53:18 -0400

    Added testing for the Transaction DAO

---
+++ src/test/java/TransactionDAOtest.java
@@ -0,0 +1,12 @@
+
+public class TransactionDAOtest {
+        @Test
+        public static void getTransactionTest() {
+                MongoCollection<Document> collection;
+                String username = "user";
+                assertTrue(TransactionDAO.getTransaction());
+                username = "admin"
+                assertTrue(TransactionDAO.adminGetTransaction())
+        }
+
+}
