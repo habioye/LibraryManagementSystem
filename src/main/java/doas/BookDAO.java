8e7ae9ccf2dd4316fe0a7e34b0ca999922bd8a1c
Hy <hyhuynh222@gmail.com>
2024-06-06 10:25:56 -0700

    Added Test Tables for DB and Added BookDAO function
    + addbook
    + viewbooks

--- src/main/java/doas/BookDAO.java
+++ src/main/java/doas/BookDAO.java
@@ -1,9 +1,16 @@
 package doas;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import database.DBConnection;
 import documents.Book;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class BookDAO {
     MongoCollection<Document> collection;
@@ -11,11 +18,44 @@ public class BookDAO {
         this.collection = new DBConnection().getCollection(collection);
     }
 
-    public void addBook(){
-
+    public void addBook(String title, String author, String description, List<String> genres){
+        Document book = new Document("title",title)
+                .append("author", author)
+                .append("description", description)
+                .append("genres", genres)
+                .append("checkedOut", false)
+                .append("currentTransactionId", "");
+        collection.insertOne(book);
     }
-    public Book getBook(){
-        return null;
+    public List<Book> getAllLibraryBooks(int mode, String name){
+        List<Book> books = new ArrayList<>();
+        String queryType = switch(mode){
+            case 1 -> "title";
+            case 2 -> "author";
+            case 3 -> "genres";
+            default -> "no";
+        };
+        Document query;
+        if (queryType.equals("genres")){
+            query = new Document(queryType, new Document("$all", Arrays.asList(name.split(" "))));
+        } else {
+            query = new Document(queryType, name);
+        }
+
+        try (MongoCursor<Document> cursor = collection.find(query).iterator()){
+            while (cursor.hasNext()){
+                Document doc = cursor.next();
+                ObjectId id = doc.getObjectId("_id");
+                String title = doc.getString("title");
+                String author = doc.getString("author");
+                String description = doc.getString("description");
+                List<String> genres = doc.getList("genres", String.class);
+                Boolean checkedOut = doc.getBoolean("checkedOut");
+                String currentTransactionId = doc.getString("currentTransactionId");
+                books.add(new Book(id.toHexString(), title, author, description, genres,checkedOut,currentTransactionId));
+            }
+        }
+        return books;
     }
 
 }
---
+++ src/test/java/SetUpTest.java
@@ -0,0 +1,40 @@
+import com.mongodb.client.MongoCollection;
+import database.DBConnection;
+import doas.BookDAO;
+import documents.Book;
+import org.bson.Document;
+
+import java.util.Arrays;
+
+public class SetUpTest {
+    // CALL THIS ONCE
+    public static void main(String[] args) {
+        BookDAO bookDAO = new BookDAO("BookTest");
+        bookDAO.addBook("Title1","Author1","Des1", Arrays.asList("Genre1","Genre2"));
+        bookDAO.addBook("Title2","Author2","Des2", Arrays.asList("Genre3","Genre2"));
+        bookDAO.addBook("Title3","Author1","Des3", Arrays.asList("Genre1","Genre4"));
+        bookDAO.addBook("Title4","Author1","Des4", Arrays.asList("Genre5","Genre3"));
+        bookDAO.addBook("Title5","Author3","Des5", Arrays.asList("Genre6","Genre7"));
+
+        DBConnection db = new DBConnection();
+        MongoCollection<Document> collection = db.getCollection("UserTest");
+        collection.insertOne(new Document("username", "user1")
+                .append("password","user1")
+                .append("role","user")
+                .append("firstname","firstname1")
+                .append("lastname","lastname1")
+                .append("checkedOutBooks",Arrays.asList()));
+        collection.insertOne(new Document("username", "user2")
+                .append("password","user2")
+                .append("role","user")
+                .append("firstname","firstname2")
+                .append("lastname","lastname2")
+                .append("checkedOutBooks",Arrays.asList()));
+        collection.insertOne(new Document("username", "admin")
+                .append("password","admin")
+                .append("role","admin")
+                .append("firstname","firstname3")
+                .append("lastname","lastname3")
+                .append("checkedOutBooks",Arrays.asList()));
+    }
+}
