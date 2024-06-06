a2a42aa8a61549bc76a545fcd5e9e3bfdac6c9d4
Hy <hyhuynh222@gmail.com>
2024-06-06 11:51:23 -0700

    Convert Methods to Static

--- src/test/java/SetUpTest.java
+++ src/test/java/SetUpTest.java
@@ -6,18 +6,20 @@ import org.bson.Document;
 
 import java.util.Arrays;
 
+import static doas.BookDAO.addBook;
+
 public class SetUpTest {
     // CALL THIS ONCE
-    public static void main(String[] args) {
-        BookDAO bookDAO = new BookDAO("BookTest");
-        bookDAO.addBook("Title1","Author1","Des1", Arrays.asList("Genre1","Genre2"));
-        bookDAO.addBook("Title1","Author1","Des1", Arrays.asList("Genre1","Genre2"));
-        bookDAO.addBook("Title2","Author2","Des2", Arrays.asList("Genre3","Genre2"));
-        bookDAO.addBook("Title3","Author1","Des3", Arrays.asList("Genre1","Genre4"));
-        bookDAO.addBook("Title4","Author1","Des4", Arrays.asList("Genre5","Genre3"));
-        bookDAO.addBook("Title5","Author3","Des5", Arrays.asList("Genre6","Genre7"));
-
+    public static void setUp(){
         DBConnection db = new DBConnection();
+        BookDAO.BookDAOInit(db.getCollection("BookTest"));
+        BookDAO.addBook("Title1","Author1","Des1", Arrays.asList("Genre1","Genre2"));
+        BookDAO.addBook("Title1","Author1","Des1", Arrays.asList("Genre1","Genre2"));
+        BookDAO.addBook("Title2","Author2","Des2", Arrays.asList("Genre3","Genre2"));
+        BookDAO.addBook("Title3","Author1","Des3", Arrays.asList("Genre1","Genre4"));
+        BookDAO.addBook("Title4","Author1","Des4", Arrays.asList("Genre5","Genre3"));
+        BookDAO.addBook("Title5","Author3","Des5", Arrays.asList("Genre6","Genre7"));
+
         MongoCollection<Document> collection = db.getCollection("UserTest");
         collection.insertOne(new Document("username", "user1")
                 .append("password","user1")
--- src/test/java/TestBookDAO.java
+++ src/test/java/TestBookDAO.java
@@ -1,4 +1,5 @@
 import com.mongodb.client.MongoCollection;
+import database.DBConnection;
 import doas.BookDAO;
 import documents.Book;
 import org.bson.Document;
@@ -11,22 +12,63 @@ import java.util.List;
 import static org.junit.Assert.*;
 public class TestBookDAO {
     @Test
-    public void testGetBooks(){
-        BookDAO bookDAO = new BookDAO("BookTest");
-        List<Book> books = bookDAO.getBookUsingFilter(1,"Title1");
-        for (Book book: books){
-            System.out.println(book);
-        }
-        System.out.println("___________________________");
-        books = bookDAO.getBookUsingFilter(2,"Author1");
-        for (Book book: books){
-            System.out.println(book);
-        }
-
-        System.out.println("___________________________");
-        books = bookDAO.getBookUsingFilter(3,"Genre2 Genre1");
-        for (Book book: books){
-            System.out.println(book);
-        }
+    public void testGetBooksFilterByTitle(){
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getCollection("BookTest").deleteMany(new Document());
+        dbConnection.getCollection("UserTest").deleteMany(new Document());
+        SetUpTest.setUp();
+        List<Book> books = BookDAO.getBookUsingFilter(1,"title1");
+        assertEquals(2,books.size());
+        assertEquals("Title1",books.get(0).getBookTitle());
+        assertEquals("Title1",books.get(1).getBookTitle());
     }
+
+    @Test
+    public void testGetBooksFilterByAUthor(){
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getCollection("BookTest").deleteMany(new Document());
+        dbConnection.getCollection("UserTest").deleteMany(new Document());
+        SetUpTest.setUp();
+        List<Book> books = BookDAO.getBookUsingFilter(2,"Author1");
+        assertEquals(4,books.size());
+        assertEquals("Author1",books.get(0).getAuthor());
+        assertEquals("Author1",books.get(1).getAuthor());
+        assertEquals("Author1",books.get(2).getAuthor());
+        assertEquals("Author1",books.get(3).getAuthor());
+
+    }
+    @Test
+    public void testGetBooksFilterByOneGenre(){
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getCollection("BookTest").deleteMany(new Document());
+        dbConnection.getCollection("UserTest").deleteMany(new Document());
+        SetUpTest.setUp();
+        List<Book> books = BookDAO.getBookUsingFilter(3,"Genre3");
+        assertEquals(2,books.size());
+        assertEquals("Title2",books.get(0).getBookTitle());
+        assertEquals("Title4",books.get(1).getBookTitle());
+    }
+    @Test
+    public void testGetBooksFilterByManyGenre(){
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getCollection("BookTest").deleteMany(new Document());
+        dbConnection.getCollection("UserTest").deleteMany(new Document());
+        SetUpTest.setUp();
+        List<Book> books = BookDAO.getBookUsingFilter(3,"Genre1 Genre2");
+        assertEquals(2,books.size());
+        assertEquals("Title1",books.get(0).getBookTitle());
+        assertEquals("Title1",books.get(1).getBookTitle());
+    }
+    @Test
+    public void testGetBooks(){
+        DBConnection dbConnection = new DBConnection();
+        dbConnection.getCollection("BookTest").deleteMany(new Document());
+        dbConnection.getCollection("UserTest").deleteMany(new Document());
+        SetUpTest.setUp();
+        List<Book> books = BookDAO.getBooks();
+        assertEquals(6,books.size());
+
+    }
+
+
 }
