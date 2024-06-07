27b99fbe5658849ee5c29a916b95afee58ab1a48
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-06 21:04:53 -0700

    added view checkouts menu

--- src/main/java/consoleUI/ViewCheckoutMenu.java
+++ src/main/java/consoleUI/ViewCheckoutMenu.java
@@ -1,4 +1,29 @@
 package consoleUI;
 
+import dao.TransactionDAO;
+import entity.Transaction;
+import entity.User;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ViewCheckoutMenu {
+
+    public static void run(Scanner sc, User user) {
+
+        ArrayList<Transaction> transactions = TransactionDAO.getTransactionsByUserId(user.getUserID());
+
+        if (transactions == null || transactions.isEmpty()) {
+            System.out.println("No checkouts found");
+        } else {
+            for (Transaction t : transactions) {
+                System.out.println(t);
+            }
+        }
+
+        System.out.println("Hit enter to return");
+        sc.nextLine();
+    }
+
 }
